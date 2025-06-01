fc613e93d0b6b621ee741cf393605b9f83760973
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-06-01 23:35:22 +0100

    Update src/data/specifications.py

--- src/data/specifications.py
+++ src/data/specifications.py
@@ -13,20 +13,7 @@ class Specifications:
         pass
 
     @staticmethod
-    def __anomalies(specifications: se.Specifications) -> se.Specifications:
-        """
-
-        :param specifications: A gauge's collection of attributes
-        :return:
-        """
-
-        specifications = specifications._replace(catchment_id=int(specifications.catchment_id),
-                                                 station_id=int(specifications.station_id),
-                                                 ts_id=int(specifications.ts_id))
-
-        return specifications
-
-    def exc(self, reference: pd.DataFrame) -> list[se.Specifications]:
+    def exc(reference: pd.DataFrame) -> list[se.Specifications]:
         """
 
         :param reference:
@@ -34,9 +21,6 @@ class Specifications:
         """
 
         dictionaries = [reference.iloc[i, :].squeeze() for i in range(reference.shape[0])]
-
         specifications_ = [se.Specifications(**dictionary) for dictionary in dictionaries]
-        specifications_ = [self.__anomalies(specifications=specifications)
-                           for specifications in specifications_]
 
         return specifications_
